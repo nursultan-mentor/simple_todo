eaaeab3ce35df04f92df6373154f5e7a2aadc7d9
nursultan-mentor <nursultanberdiev1994@gmail.com>
2022-09-27 13:23:18 +0600

    added static root

--- todo_proj/urls.py
+++ todo_proj/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from todo.views import TodoViewSet
+from todo_proj import settings
+
 
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet)
@@ -10,4 +13,4 @@ router.register(r'todos', TodoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
