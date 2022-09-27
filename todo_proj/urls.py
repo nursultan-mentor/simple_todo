0a4efc93030317f42f925b198248b5310f5eeb13
nursultan-mentor <nursultanberdiev1994@gmail.com>
2022-09-27 14:13:02 +0600

    fix Procfile

--- todo_proj/urls.py
+++ todo_proj/urls.py
@@ -2,15 +2,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
 
 from todo.views import TodoViewSet
-from todo_proj import settings
-
 
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('api/', include(router.urls)),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
